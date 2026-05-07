5858e87c1f5b338ae8cc9574129b9be0a8aba66f
pilo77 <cavillamil-2022a@corhuila.edu.co>
2026-05-07 11:39:14 -0500

    chore(HU-096): remove sensitive audit files from version control

--- .copilot_tmp_db_audit/fix_08_types_after.sql
+++
@@ -1,8 +0,0 @@
-DO '
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = ''product_status'') THEN
-    CREATE TYPE product_status AS ENUM (''ACTIVE'', ''INACTIVE'', ''DISCONTINUED'');
-  END IF;
-END
-';
-
